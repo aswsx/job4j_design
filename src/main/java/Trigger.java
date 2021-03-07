7dc8ac4e4141f1a37256600c945102313e1c5756
Tosh-PC <lextor78@gmail.com>
2021-03-07 20:48:53 +0600

    codecov

---
+++ src/main/java/Trigger.java
@@ -0,0 +1,5 @@
+public class Trigger {
+    public int someLogic() {
+        return 1;
+    }
+}
---
+++ src/test/java/TriggerTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TriggerTest {
+
+    @Test
+    public void test() {
+        Assert.assertEquals(1, new Trigger().someLogic());
+    }
+
+}
