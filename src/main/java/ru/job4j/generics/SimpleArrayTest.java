2855b9a196d922cb59c64f4490cd4f0494bb9bc9
Tosh-PC <lextor78@gmail.com>
2021-03-12 18:19:21 +0600

    corr

--- src/main/java/ru/job4j/generics/SimpleArrayTest.java
+++ src/main/java/ru/job4j/generics/SimpleArrayTest.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class SimpleArrayTest {
