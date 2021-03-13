8b6389a737acc51807cf898a7f034197ca66cfea
Tosh-PC <lextor78@gmail.com>
2021-03-13 11:29:52 +0600

    5.2.2. Реализовать Store<T extends Base> [#488335]

--- src/main/java/ru/job4j/generics/MemStore.java
+++ src/main/java/ru/job4j/generics/MemStore.java
@@ -23,12 +23,21 @@ public final class MemStore<T extends Base> implements Store<T> {
     }
 
     private boolean elemAction(String id, T model) {
-        T found = findById(id);
-        if (found == null) {
+        int index = findIndex(id);
+        if (index == -1) {
             return false;
         }
-        mem.set(mem.indexOf(found), model);
+        mem.set(index, model);
         return true;
+    }
+
+    private int findIndex(String id) {
+        for (int i = 0; i < mem.size(); i++) {
+            if (mem.get(i).getId().equals(id)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
