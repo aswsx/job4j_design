afa88e48b52d96c5d2158bd034f827d0dd56e993
Tosh-PC <lextor78@gmail.com>
2021-04-03 17:58:19 +0600

    3. Переопределить только hashCode

--- src/main/java/ru/job4j/collection/hash/User.java
+++ src/main/java/ru/job4j/collection/hash/User.java
@@ -1,9 +1,6 @@
 package ru.job4j.collection.hash;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class User {
     private final String name;
@@ -16,15 +13,33 @@ public class User {
         this.birthday = birthday;
     }
 
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (!(o instanceof User)) return false;
+//        User user = (User) o;
+//        return children == user.children && Objects.equals(name, user.name)
+//        && Objects.equals(birthday, user.birthday);
+//    }
+
+    @SuppressWarnings("checkstyle:EqualsHashCode")
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
     public static void main(String[] args) {
-
+        Calendar birthday = new GregorianCalendar(1985,
+                Calendar.MARCH,
+                25,
+                18,
+                0,
+                0);
+        birthday.set(Calendar.MILLISECOND, 0);
         User first = new User("Alex",
-                2,
-                new GregorianCalendar(1985, Calendar.MARCH, 25));
+                2, birthday);
         User second = new User("Alex",
-                2,
-                new GregorianCalendar(1985, Calendar.MARCH, 25));
-
+                2, birthday);
         Map<User, Object> map = new HashMap<>();
         map.put(first, new Object());
         map.put(second, new Object());
