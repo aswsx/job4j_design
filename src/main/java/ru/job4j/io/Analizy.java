0b4731c28a9e6d661e0880c8fc5f2e4e3ab2bc72
Tosh-PC <lextor78@gmail.com>
2021-04-19 21:32:06 +0600

    2. Анализ доступности сервера.

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,42 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.PrintWriter;
+
+/**
+ * @author Alex Gutorov
+ * @version 1.0
+ */
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             PrintWriter write = new PrintWriter(new FileOutputStream(target))) {
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = read.readLine()) != null) {
+                String[] list = line.split(" ");
+                if (builder.length() == 0 && (list[0].equals("400") || list[0].equals("500"))) {
+                    builder.append(list[1]).append(";");
+                }
+                if (builder.length() != 0 && (list[0].equals("200") || list[0].equals("300"))) {
+                    builder.append(list[1]).append(";");
+                    write.println(builder);
+                    builder = new StringBuilder();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.*;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void serverLogAnalyze() throws IOException {
+        File source = folder.newFile("server.log");
+        File target = folder.newFile("unavailable.csv");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("300 08:21:01");
+            out.println("300 08:57:01");
+            out.println("400 08:58:01");
+            out.println("400 09:59:01");
+            out.println("500 10:01:02");
+            out.println("500 10:02:02");
+            out.println("300 10:56:01");
+            out.println("400 10:57:01");
+            out.println("300 10:58:01");
+            out.println("200 10:59:01");
+            out.println("400 12:01:02");
+            out.println("400 14:02:02");
+            out.println("500 14:21:01");
+            out.println("200 14:59:02");
+            out.println("300 15:11:01");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder result = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+            reader.lines().forEach(result::append);
+        }
+        assertThat(result.toString(),
+                is("08:58:01;10:56:01;10:57:01;10:58:01;12:01:02;14:59:02;"));
+    }
+}
