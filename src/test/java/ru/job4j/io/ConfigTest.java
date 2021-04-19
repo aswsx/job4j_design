fe229d213a7fa93de1da37559f0babc066a25c96
Tosh-PC <lextor78@gmail.com>
2021-04-19 22:17:25 +0600

    corr

--- src/test/java/ru/job4j/io/ConfigTest.java
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -2,32 +2,34 @@ package ru.job4j.io;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.is;
+import java.net.URL;
+
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertNull;
 
 public class ConfigTest {
 
     @Test
     public void whenPairWithoutComment() {
-        String path = "pair_without_comment.properties";
-        Config config = new Config(path);
+        URL path = ClassLoader.getSystemResource("pair_without_comment.properties");
+        Config config = new Config(path.getPath());
         config.load();
         assertThat(config.value("name"), is("Alex Gutorov"));
     }
 
     @Test
     public void whenPairWithComment() {
-        String path = "pair_with_comment.properties";
-        Config config = new Config(path);
+        URL path = ClassLoader.getSystemResource("pair_with_comment.properties");
+        Config config = new Config(path.getPath());
         config.load();
         assertThat(config.value("name"), is("Alex Gutorov"));
     }
 
     @Test
     public void whenPairIsEmpty() {
-        String path = "pair_empty.properties";
-        Config config = new Config(path);
+        URL path = ClassLoader.getSystemResource("pair_empty.properties");
+        Config config = new Config(path.getPath());
         config.load();
         assertNull(config.value("name"));
     }
