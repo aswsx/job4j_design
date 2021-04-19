613e1417bb9b522718e8593e1635770bd671ecd1
Tosh-PC <lextor78@gmail.com>
2021-04-19 23:59:49 +0600

    corr

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -30,13 +30,4 @@ public class Analizy {
             e.printStackTrace();
         }
     }
-
-    //  public static void main(String[] args) {
-//        try (PrintWriter out = new PrintWriter(new FileOutputStream("resources/unavailable.csv"))) {
-////            out.println("15:01:30;15:02:32");
-////            out.println("15:10:30;23:12:32");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-    //  }
 }
