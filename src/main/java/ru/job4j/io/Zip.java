eaa4336542e8af21c936f4b677c919f013314424
Tosh-PC <lextor78@gmail.com>
2021-05-09 21:16:07 +0600

    corr

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,45 @@
+package ru.job4j.io;
+
+import ru.job4j.io.duplicates.DuplicatesVisitor;
+
+import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public void packFiles(List<File> sources, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (File source : sources) {
+                zip.putNextEntry(new ZipEntry(source.getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        new Zip().packSingleFile(
+                new File.,
+                new File("./chapter_005/pom.zip")
+        );
+        new Zip().packFiles(visitor.visitFile(new File("."),  ),
+                new File("./chapter_005/pom.zip");
+    }
+}
