a4ba26dbabd822825cf5d66b60f0a5434d98cda2
Tosh-PC <lextor78@gmail.com>
2021-05-16 13:42:13 +0600

    1. Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,8 +1,22 @@
 package ru.job4j.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+
+/**
+ * Класс - реализация простого сокета
+ *
+ * @author Alex Gutorov
+ * @version 1.1
+ * При получении запроса, содержащего Hello выводится в консоль Hello
+ * При получении запроса, содержащего Exit выполняется остановка сервера
+ * При получении любого другого запроса выводится в консоль What
+ *
+ */
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -14,9 +28,12 @@ public class EchoServer {
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
                     while (!(str = in.readLine()).isEmpty()) {
-                        System.out.println(str);
-                        if (str.contains("Bye")) {
+                        if (str.contains("Exit")) {
                             server.close();
+                        } else if (str.startsWith("GET") && !str.contains("Hello")) {
+                            System.out.println("What");
+                        } else if (str.startsWith("GET") && str.contains("Hello")) {
+                            System.out.println("Hello");
                         }
                     }
                     out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
