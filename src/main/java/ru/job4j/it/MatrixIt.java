ea8ecf26f30f7c4d3e88966237ee4870c712df25
Tosh-PC <lextor78@gmail.com>
2021-06-04 23:59:47 +0600

    corr

--- src/main/java/ru/job4j/it/MatrixIt.java
+++ src/main/java/ru/job4j/it/MatrixIt.java
@@ -3,10 +3,11 @@ package ru.job4j.it;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-public class MatrixIt implements Iterator<Integer> {
+public class
+MatrixIt implements Iterator<Integer> {
     private final int[][] data;
-    private int row = 0;
-    private int column = 0;
+    private int row = 0; //Индекс строки
+    private int column = 0; //индекс элемента строки
 
     public MatrixIt(int[][] data) {
         this.data = data;
@@ -15,10 +16,16 @@ public class MatrixIt implements Iterator<Integer> {
     @Override
     public boolean hasNext() {
         while (row < data.length && data[row].length == column) {
-            row++;
-            column = 0;
+            // запускаем цикл, проверяем-
+            // индекс строки меньше размера массива?
+            // И длина текущей строки равна текущему индексу элемента в строке?
+            row++; // ЕСЛИ ДА, шагаем в следующую строку
+            column = 0; // индекс элемента строки обнуляем
+            // и снова проверяем
         }
-        return row < data.length;
+        return row < data.length; //ЕСЛИ НЕТ возвращаем булево значение-
+                                  // индекс строки меньше размера массива
+        // если меньше- true, равен - false
     }
 
     @Override
