b9793d20d53a2f3a063519bd848ee395bd14a24d
Tosh-PC <lextor78@gmail.com>
2021-06-23 22:25:10 +0600

    0.1. Statement

---
+++ src/main/java/ru/job4j/jdbc/StatementDemo.java
@@ -0,0 +1,43 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+
+public class StatementDemo {
+
+    private static Connection getConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        var url = "jdbc:postgresql://localhost:5432/idea_db";
+        var login = "postgres";
+        var password = "password";
+        return DriverManager.getConnection(url, login, password);
+    }
+
+    public static void main(String[] args) throws Exception {
+        try (var connection = getConnection()) {
+            try (var statement = connection.createStatement()) {
+                var sql = String.format(
+                        "create table if not exists demo_table(%s, %s);",
+                        "id serial primary key",
+                        "name varchar(255)"
+                );
+                statement.execute(sql);
+                System.out.println(getTableScheme(connection, "demo_table"));
+            }
+        }
+    }
+
+    public static String getTableScheme(Connection connection, String tableName) throws Exception {
+        var scheme = new StringBuilder();
+        DatabaseMetaData metaData = connection.getMetaData();
+        try (ResultSet columns = metaData.getColumns(null, null, tableName, null)) {
+            scheme.append(String.format("%-15s %-15s%n", "column", "type"));
+            while (columns.next()) {
+                scheme.append(String.format("%-15s %-15s%n",
+                        columns.getString("COLUMN_NAME"),
+                        columns.getString("TYPE_NAME")));
+            }
+        }
+        return scheme.toString();
+    }
+
+}
---
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -0,0 +1,90 @@
+package ru.job4j.jdbc;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class TableEditor implements AutoCloseable {
+
+    private Connection connection;
+
+    private final Properties properties;
+    private static final String ALTER_TABLE = "alter table"; //избавляет от дублирования кода
+
+    public TableEditor(Properties properties) throws SQLException, ClassNotFoundException {
+        this.properties = properties;
+        initConnection();
+    }
+
+    private void initConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.postgresql.Driver");
+        connection = DriverManager.getConnection(
+                properties.getProperty("url"),
+                properties.getProperty("login"),
+                properties.getProperty("password")
+        );
+    }
+
+    public void createTable(String tableName) throws SQLException {
+        String sql = "create table if not exists " + tableName + "()";
+//        var sql = String.format(
+//                "create table if not exists %s (%s, %s);",
+//                tableName, "id serial primary key",
+//                "name varchar(255)"
+//        );
+        execute(sql);
+    }
+
+    public void dropTable(String tableName) throws SQLException {
+        String sql = "drop table if exists " + tableName;
+        execute(sql);
+    }
+
+    public void addColumn(String tableName, String columnName, String type) throws SQLException {
+        String sql = ALTER_TABLE + " " + tableName + " add column " + columnName + " " + type;
+        execute(sql);
+    }
+
+    public void dropColumn(String tableName, String columnName) throws SQLException {
+        String sql = ALTER_TABLE + " " + tableName + " drop column " + columnName;
+        execute(sql);
+    }
+
+    public void renameColumn(String tableName, String columnName, String newColumnName) throws SQLException {
+        String sql = ALTER_TABLE + " " + tableName + " rename column " + columnName + " to " + newColumnName;
+        execute(sql);
+    }
+
+    public String getScheme(String tableName) throws SQLException {
+        var scheme = new StringBuilder();
+        DatabaseMetaData metaData = connection.getMetaData();
+        try (ResultSet columns = metaData
+                .getColumns(null, null, tableName, null)) {
+            scheme.append(String.format("%-15s %-15s%n", "column", "type"));
+            while (columns.next()) {
+                scheme.append(String.format("%-15s %-15s%n",
+                        columns.getString("COLUMN_NAME"),
+                        columns.getString("TYPE_NAME")));
+            }
+        }
+        return scheme.toString();
+    }
+
+     /**
+     * избавляет от дублирования кода
+     *
+     * @param sql
+     * @throws SQLException
+     */
+    private void execute(String sql) throws SQLException {
+        var statement = connection.createStatement();
+        statement.execute(sql);
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        if (connection != null) {
+            connection.close();
+        }
+    }
+}
