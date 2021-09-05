305617a63fa661187399aa48cfdf3bf0e9d0e0e7
Tosh-PC <lextor78@gmail.om>
2021-09-05 19:26:51 +0600

    c8n

--- src/test/java/ru/job4j/collection/SimpleArrayTest.java
+++ src/test/java/ru/job4j/collection/SimpleArrayTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.collection;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ConcurrentModificationException;
@@ -12,9 +13,15 @@ import static org.junit.Assert.assertNull;
 
 public class SimpleArrayTest {
 
+    private static SimpleArray<String> array;
+
+    @Before
+    public void createArray() {
+        array = new SimpleArray<>();
+    }
+
     @Test
     public void whenAddThenGet() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         String rsl = array.get(0);
         assertThat(rsl, is("first"));
@@ -22,7 +29,6 @@ public class SimpleArrayTest {
 
     @Test
     public void whenAddThenIt() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         String rsl = array.iterator().next();
         assertThat(rsl, is("first"));
@@ -38,26 +44,22 @@ public class SimpleArrayTest {
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenGetEmpty() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.get(0);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void whenGetOutBound() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         array.get(1);
     }
 
     @Test(expected = NoSuchElementException.class)
     public void whenGetEmptyFromIt() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.iterator().next();
     }
 
     @Test(expected = ConcurrentModificationException.class)
     public void whenCorruptedIt() {
-        SimpleArray<String> array = new SimpleArray<>();
         array.add("first");
         Iterator<String> it = array.iterator();
         array.add("second");
