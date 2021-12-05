9d450b2aa3c01a807b66dbd74ae381947d0a3b87
Tosh-PC <lextor78@gmail.com>
2021-12-05 13:47:53 +0600

    3. Шаблонизатор.

--- src/main/java/ru/job4j/template/PhraseGenerator.java
+++ src/main/java/ru/job4j/template/PhraseGenerator.java
@@ -2,7 +2,7 @@ package ru.job4j.template;
 
 import java.util.Map;
 
-public class PhraseGenerator implements Generator{
+public class PhraseGenerator implements Generator {
     @Override
     public String produce(String template, Map<String, String> args) {
         return null;
